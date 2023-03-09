7a162c8fb2e248aa792744238505ecde1caa83f4
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-03-09 10:42:27 +0100

    Added documentation

--- src/ft_count_chars.c
+++ src/ft_count_chars.c
@@ -6,12 +6,14 @@
 /*   By: arabenst <arabenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 10:34:14 by arabenst          #+#    #+#             */
-/*   Updated: 2023/03/09 10:35:43 by arabenst         ###   ########.fr       */
+/*   Updated: 2023/03/09 10:42:09 by arabenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// RETURN
+// Number of occurences of the char ’c’ in ’str’
 size_t	ft_count_chars(char *str, char c)
 {
 	size_t	count;
