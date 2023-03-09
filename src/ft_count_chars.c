70946323cd82caf41a7871eb28f8c168545169f8
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-03-09 10:36:15 +0100

    Added count_char function

---
+++ src/ft_count_chars.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_chars.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabenst <arabenst@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 10:34:14 by arabenst          #+#    #+#             */
+/*   Updated: 2023/03/09 10:35:43 by arabenst         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_count_chars(char *str, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
